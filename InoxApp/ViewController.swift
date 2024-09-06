2d04904a69e63d2aee451c906c69572601210814
Gembali Sandesh Kumar <93411433+gembalisandesh@users.noreply.github.com>
2024-09-06 21:17:34 +0530

    Initial Commit

---
+++ InoxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InoxApp
+//
+//  Created by Equipp on 06/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
